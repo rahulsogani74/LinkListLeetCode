d97609687d701c3235c2d630dc0377891cf1b9fa
rahulsogani74 <131875485+rahulsogani74@users.noreply.github.com>
2023-07-13 00:12:36 +0530

    Add files via upload

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,43 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode result;
+        if (l1.val <= l2.val) {
+            result = l1;
+            result.next = mergeTwoLists(l1.next, l2);
+        } else {
+            result = l2;
+            result.next = mergeTwoLists(l1, l2.next);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+
+        MergeTwoSortedLists merger = new MergeTwoSortedLists();
+        ListNode mergedList = merger.mergeTwoLists(l1, l2);
+
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+}
